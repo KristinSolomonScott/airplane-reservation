30751c7328e7bd96b500e9489452f2fb6c851da8
Kristin Scott <107061559+KristinSolomonScott@users.noreply.github.com>
2022-07-21 04:02:55 +0000

    final

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+'''
+Airline Reservation system - for employee use only
+by Kristn Scott
+July 2022
+Indiana University
+CSCI T510 – Intro to Comp and Prog for K-12 Teachers
+'''
 
-
+#prints options that users can choose
 def print_options():
   print("------------------------------------------------")
   print("A. Assign a seat to a passenger.")
@@ -9,16 +16,19 @@ def print_options():
   print("E. Quit. ")
   print("------------------------------------------------")
 
-
+#variable to keep track of availability in first class
 global first_full
 first_full=False
 
+#variable to keep track of availability in business class
 global business_full
 business_full=False
 
+#variable to keep track of availability in economy class
 global economy_full
 economy_full=False
 
+#items are added to the assignments list when a seat is assigned. "assignments" holds lists of two. The first element in the list is the seat number and the second element is the passenger's name. 
 assignments=[]
 '''This function  (1) adds one name and seat number to the "assignments" list, (2) deletes the name from the "passengers" list, and (3) marks the seat as taken in the "seats" list. '''
 def assign_seat():
@@ -27,9 +37,40 @@ def assign_seat():
     if section.lower().strip()=="a":
       global first_full
       if first_full==False:
+        assign_fc()  
+      else:
+        next_best=input("First class is full. Would you like a seat in Business Class? Key \"Y\" for YES or any other key for NO \n")
+        if next_best.upper().strip()=="Y":
+          assign_bc()
+       
+          
+    elif section.lower().strip()=="b":
+      global business_full
+      if business_full==False:
+        assign_bc()
+      else:
+        next_best=input("Business class is full. Would you like a seat in Economy Class? Key \"Y\" for YES or any other key for NO \n")
+        if next_best.upper().strip()=="Y":
+          assign_ec()
+          
+    elif section.lower().strip()=="c":
+      global economy_full
+      if economy_full==False:
+        assign_fc()
+      else:
+        print("Economy section is full.")
+  else:
+    print("***********************************************************")
+    print("There are no passengers to assign. Please choose another option.")
+    print("***********************************************************")
+    print()
+
+
+def assign_fc():
+        global first_full
         for i in range(len(seats)):
           if (seats[i][0][1].endswith("1") or seats[i][0][1].endswith("2") or seats[i][0][1].endswith("3")) and seats[i][1]==0:
-            assignments.append([seats[i],passengers[0]]) #add name & seat number to "assignments" list
+            assignments.append([seats[i][0],passengers[0]]) #add name & seat number to "assignments" list
             ##
             print("////////////")
             print("Assignments list:\n")
@@ -42,63 +83,75 @@ def assign_seat():
             if seats[20][1]=="X":
               first_full=True
             break
-      else:
-        print("First class is full.")
-  
-  
-          
-    elif section.lower().strip()=="b":
-      global business_full
-      if business_full==False:
-        for i in range(len(seats)):
+
+
+def assign_bc():
+  global business_full
+  for i in range(len(seats)):
           if (seats[i][0][1].endswith("4") or seats[i][0][1].endswith("5") or seats[i][0][1].endswith("6")) and seats[i][1]==0:
-            assignments.append([seats[i],passengers[0]]) #add name & seat number to "assignments" list
-            ##
-            print("////////////")
-            print("Assignments list:\n")
-            for item in assignments:
-              print(item , "\n")
-            print("////////////")
-              ##
-            
+            assignments.append([seats[i][0],passengers[0]]) #adds name & seat number to "assignments" list
+                        
             del passengers[0] #remove name from passengers list after seat is assigned
             seats[i][1]="X" #change seat to show as taken in "seats" list
             if seats[41][1]=="X":
               business_full=True
             break
-      else:
-        print("Business section is full.")
   
-          
-    elif section.lower().strip()=="c":
-      global economy_full
-      if economy_full==False:
-        for i in range(len(seats)):
+
+def assign_ec():
+  global economy_full
+  for i in range(len(seats)):
           if i>41 and seats[i][1]==0:
-            assignments.append([seats[i],passengers[0]])
-  
-  
-            ##
-            print("////////////")
-            print("Assignments list:\n")
-            for item in assignments:
-              print(item , "\n")
-            print("////////////")
-              ##
+            assignments.append([seats[i][0],passengers[0]])
+
             del passengers[0]
             seats[i][1]="X" 
             if seats[139][1]=="X":
               economy_full=True
             break
-      else:
-        print("Economy section is full.")
-  else:
-    print("***********************************************************")
-    print("There are no passengers to assign. Please choose another option.")
-    print("***********************************************************")
-    print()
+            
 
+#prints the name, seat number, and class for a given seat number
+def boarding_pass():
+  bp_done=False
+  seat_no=""
 
+  while not bp_done:
+ 
+    seat_no=input("Please enter a valid seat number or enter Q to return to the main menu.")
+  
+    if seat_no.lower().strip()=="q":   
+      bp_done=True
+      break
+
+    else:
+      for i in range(len(assignments)):
+            pclass=""
+            
+            if assignments[i][0]==seat_no.upper().strip():
+              if int(assignments[i][0][1:])<3:
+                pclass="First Class"
+              elif int(assignments[i][0][1:])<6:
+                pclass="Business Class"
+              else:
+                pclass="Economy Class"
+        
+              print()
+              print()
+              print("- - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+              print("- - - - - - - - - -  BOARDING PASS  - - - - - - - - - -")
+              
+              print("Seat: ",assignments[i][0])
+              print("Section: ", pclass)
+              print("Passenger Name: ", assignments[i][1])
+        
+              print("- - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+              print()
+              print()
+              
+              bp_done=True
+
+#prints all assigned seat numbers and passenger names (either ordered by seat numbers or alphabetically by passenger name)
 def manifest():
   alpha=input("Would you like to print the list alphabetically \n by passengers' last names? If yes, type \"Y\". \nIf no, type any other key. \n --------------------------------------------- \n ")
   if alpha.lower().strip()=="y":
@@ -107,18 +160,16 @@ def manifest():
     print("---------------------------------------------")
     alpha_passengers=sorted(assignments, key=lambda x: x[1])
     for i in range(len(alpha_passengers)):
-      print(alpha_passengers[i][0][0],"\t\t\t", alpha_passengers[i][1])
+      print(alpha_passengers[i][0],"\t\t\t", alpha_passengers[i][1])#print(alpha_passengers[i][0][0],"\t\t\t", alpha_passengers[i][1])
     print()
   else:
     print()
     print("Seat Number \t\t\t Passenger Name")
     for i in range(len(assignments)):
-      print(assignments[i][0][0], "\t\t\t", assignments[i][1])
+      print(assignments[i][0], "\t\t\t", assignments[i][1])#print(assignments[i][0][0], "\t\t\t", assignments[i][1])
     print()
 
-
-
-
+#prints a visual representation of all seats showing "X" for seats that are unavailable and "0" for seats that are available
 def status_report():
   place=0
   for x in range(20):
@@ -130,31 +181,25 @@ def status_report():
           place+=1
       print()
 
-
-
-
-
 done=False
-
 
 passengers= ["Elliott, Jen", "Blocher, Emily", "Lohr, Jennifer", "Metzmeier, Angie", "Dillon, Heather", "Rose, Kristin","Miller, Ronda", "Coomer, Kelly", "Allen, Christina", "Dean, Cindy", "Allen, Becky","Fowler, Beth","Garcia, Angela", "McMillin, Denise", "Tally, Laura", "Breen, Jen", "Scott, Kim", "Walker, Stephanie", "Walker, Michelle", "Bliss, Scott", "Hisey, Debbie", "Hisey, Nancy", "Utz, Beth", "Endres, Erin", "Hall, Joyce", "Scott, Peggy", "Solomon, Jane", "Solomon, Winston", "Helton, Indy" ]
 
 
 seats = [["A1", 0], ["B1",0], ["C1",0],["D1",0],["E1",0],["F1",0],["G1",0],["A2", 0], ["B2",0], ["C2",0],["D2",0],["E2",0],["F2",0],["G2",0], ["A3", 0], ["B3",0], ["C3",0],["D3",0],["E3",0],["F3",0],["G3",0], ["A4", 0], ["B4",0], ["C4",0],["D4",0],["E4",0],["F4",0],["G4",0], ["A5", 0], ["B5",0], ["C5",0],["D5",0],["E5",0],["F5",0],["G5",0], ["A6", 0], ["B6",0], ["C6",0],["D6",0],["E6",0],["F6",0],["G6",0], ["A7", 0], ["B7",0], ["C7",0],["D17",0],["E7",0],["F7",0],["G7",0], ["A8", 0], ["B8",0], ["C8",0],["D8",0],["E8",0],["F8",0],["G8",0], ["A9", 0], ["B9",0], ["C9",0],["D9",0],["E9",0],["F9",0],["G9",0], ["A10", 0], ["B10",0], ["C10",0],["D10",0],["E10",0],["F10",0],["G10",0], ["A11", 0], ["B11",0], ["C11",0],["D11",0],["E11",0],["F11",0],["G11",0], ["A12", 0], ["B12",0], ["C12",0],["D12",0],["E12",0],["F12",0],["G12",0], ["A13", 0], ["B13",0], ["C13",0],["D13",0],["E13",0],["F13",0],["G13",0],["A14", 0], ["B14",0], ["C14",0],["D14",0],["E14",0],["F14",0],["G14",0], ["A15", 0], ["B15",0], ["C15",0],["D15",0],["E15",0],["F15",0],["G15",0], ["A16", 0], ["B16",0], ["C16",0],["D16",0],["E16",0],["F16",0],["G16",0], ["A17", 0], ["B17",0], ["C17",0],["D17",0],["E17",0],["F17",0],["G17",0], ["A18", 0], ["B18",0], ["C18",0],["D18",0],["E18",0],["F18",0],["G18",0] , ["A19", 0], ["B19",0], ["C19",0],["D19",0],["E19",0],["F19",0],["G19",0], ["A20", 0], ["B20",0], ["C20",0],["D20",0],["E20",0],["F20",0],["G20",0]]
 
-
+#Marks seats unavailable for Covid-19 protocol
+for i in range(len(seats)):
+    if seats[i][0][0]=="B" or seats[i][0][0]=="D" or seats[i][0][0]=="F":
+      seats[i][1]="X"
 
 while not done:
   print_options()
   option=input("Please type a letter to indicate your choice of options.\n")
   if option.lower().strip()=="a":
-    print("Number of items in \"seats\" list:", len(seats))  ###
-    print("Number of items in \"assignments\" list:", len(assignments))  ###
-    print("Number of items in \"passengers\" list:", len(passengers))  ###
     assign_seat()
-      
   elif option.lower().strip()=="b":
-    print()#Print a boarding pass
+    boarding_pass()
   elif option.lower().strip()=="c":
     status_report() #Seating status report
   elif option.lower().strip()=="d":
